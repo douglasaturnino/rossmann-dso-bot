49c92358254a2411a3a5c10283308eed43fcab35
Douglas Saturnino <douglasaturnino@gmail.com>
2022-01-02 15:12:42 -0300

    inicial commit
    inicialize commit
    
    inicialize commit
    
    inicial commit

--- rossmann-bot.py
+++ rossmann-bot.py
@@ -11,7 +11,6 @@ def send_message(chat_id, text):
     url= 'https://api.telegram.org/bot{}/'.format(token)
     url = url + 'sendMessage?chat_id={}'.format(chat_id)  
 
-    print(url)
     r = requests.post(url, json={'text':text})
     print('Status Code {}'.format(r.status_code))
     
